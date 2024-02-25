a2d62e250a8da500696caddb3688b66bc448fbe6
Ferris Kleier <ferris.kleier@objective-partner.com>
2024-02-25 19:43:30 +0100

    changed user handling

--- bot/config.go
+++ bot/config.go
@@ -1,11 +1,11 @@
 package bot
 
-// REPLACE WITH YOUR CHATID
+// AuthorizedUsers REPLACE WITH YOUR CHATID
 // Start servic and check output when messaging bot to see your id
 // You will appear as not authorized in the console, but can see your id
 var AuthorizedUsers = map[int]struct{}{
 	0000000: {},
 }
 
-// To get notified if unauthorized users try to access your bot
+// MainUser To get notified if unauthorized users try to access your bot
 var MainUser int64 = 0000000
